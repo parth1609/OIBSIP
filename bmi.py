a53ed385b46d59215ae7ea7bce46a7a9c8d7819a
parth1694 <115941606+parth1609@users.noreply.github.com>
2024-02-17 00:36:06 +0530

    Update bmi.py
    document the action

--- bmi.py
+++ bmi.py
@@ -1,21 +1,41 @@
+# The above class calculates the BMI (Body Mass Index) based on the given height and weight, and
+# determines the weight status (low, medium, or overweight) based on the calculated BMI.
 class bmi:
     def __init__(self, height=None, weight=None, val=None):
+        """
+        The function is a constructor that initializes the height, weight, and val attributes of an
+        object.
+        
+        """
         self.height = height
         self.weight = weight
         self.val = val
 
     def calculate(self):
+        """
+        The function calculates the body mass index (BMI) using the weight and height values.
+        :return: The value of `self.val` is being returned.
+        """
         self.val = self.weight / (self.height ** 2)
         return self.val
 
     def Health(self, val):
-        if val <= 15:
-            return "low"
-        elif val >= 15 and val <= 30:
-            return "medium"
+        """
+        The function determines whether a person's health is underweight, healthy, or overweight based
+        on their input value.
+        """
+        if val < 18.5:
+            return "You are Underweight"
+        elif val >= 18.5 and val <= 24.9:
+            return "You are healthy "
         else:
-            return "overweight"
+            return "You are overweight"
 
+        """
+        The main function calculates the BMI based on the given height and weight and returns the weight
+        status.
+        """
+        
     @staticmethod
     def main(height, weight):
         bmi_obj = bmi(height=height, weight=weight)
@@ -23,7 +43,7 @@ class bmi:
         weight_status = bmi_obj.Health(bmi_val)
         return weight_status
 
-# Example usage:
+
 height = float(input("Please enter the Height in meter"))
 weight = int(input("Please enter the Weight in kg"))
 weight_status = bmi.main(height, weight)
